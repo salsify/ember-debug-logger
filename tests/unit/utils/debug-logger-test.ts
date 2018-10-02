9d763614f99e14b12a4eeb502525b4bcde830789
Dan Freeman <dfreeman@salsify.com>
2018-10-02 12:35:18 -0400

    Remove automatic injection behavior

--- tests/unit/utils/debug-logger-test.ts
+++ tests/unit/utils/debug-logger-test.ts
@@ -4,38 +4,38 @@ import debugLogger from 'ember-debug-logger';
 import { module, test } from 'qunit';
 import sinon, { SinonStub } from 'sinon';
 
-let log: SinonStub;
+module('Unit | Utility | debug logger', function(hooks) {
+  let log: SinonStub;
 
-module('Unit | Utility | debug logger', {
-  beforeEach() {
+  hooks.beforeEach(function() {
     debug.enable('test:sample-key');
     log = sinon.stub(console, 'log');
-  },
+  });
 
-  afterEach() {
+  hooks.afterEach(function() {
     debug.disable();
     log.restore();
-  },
-});
+  });
 
-test('it honors an explicit key', function(assert) {
-  const logger = debugLogger('test:sample-key');
-  logger('placeholder message');
+  test('it honors an explicit key', function(assert) {
+    const logger = debugLogger('test:sample-key');
+    logger('placeholder message');
 
-  let [message] = A(log.args).get('lastObject') || [''];
-  assert.ok(/test:sample-key/.test(message), 'Log entry should contain the namespace');
-  assert.ok(/placeholder message/.test(message), 'Log entry should contain the message');
-});
+    let [message] = A(log.args).get('lastObject') || [''];
+    assert.ok(/test:sample-key/.test(message), 'Log entry should contain the namespace');
+    assert.ok(/placeholder message/.test(message), 'Log entry should contain the message');
+  });
 
-test('it throws with a useful message when it can\'t determine a key', function(assert) {
-  const logger = debugLogger();
+  test('it throws with a useful message when it can\'t determine a key', function(assert) {
+    const logger = debugLogger();
 
-  assert.throws(function() {
-    logger('message');
-  }, /explicit key/);
+    assert.throws(function() {
+      logger('message');
+    }, /explicit key/);
 
-  assert.throws(function() {
-    let object = { debug: logger };
-    object.debug('message');
-  }, /explicit key/);
+    assert.throws(function() {
+      let object = { debug: logger };
+      object.debug('message');
+    }, /explicit key/);
+  });
 });
